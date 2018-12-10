d43b61d70c35b4ab0d41480ad8dc8fc78b207819
Mark Stahl <mjstahl@gmail.com>
2018-12-10 13:47:07 -0500

    Added validation to token input field

--- components/authenticate/authenticate.js
+++ components/authenticate/authenticate.js
@@ -2,12 +2,19 @@ import { Component } from 'can'
 import APIInfo from '~/models/api-info'
 import view from './authenticate.stache'
 
+const TOKEN_VALIDATE = /\b[0-9a-f]{5,40}\b/
+
 Component.extend({
   tag: 'pto-authenticate',
   view,
   ViewModel: {
     apiInfo: {
       Type: APIInfo
+    },
+
+    validToken: {
+      default: true,
+      type: 'boolean'
     },
 
     token: {
@@ -25,9 +32,18 @@ Component.extend({
       }
     },
 
-    authenticate () {
-      this.apiInfo.token = this.token
-      this.apiInfo.url = this.url
-    }
+    authenticate (ev) {
+      ev.preventDefault()
+
+      if (TOKEN_VALIDATE.test(this.token)) {
+        this.validToken = true
+
+        this.apiInfo.token = this.token
+        this.apiInfo.url = this.url
+      } else {
+        this.validToken = false
+        document.querySelector('input#token').select()
+      }
+    },
   }
 })
